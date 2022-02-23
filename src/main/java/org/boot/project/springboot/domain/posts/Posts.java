f350c3923637d4fed3d50efcc85227b046452795
hyunmin0317 <choihm9903@naver.com>
2022-02-23 17:46:41 +0900

    feat: Spring JPA (124p)

--- src/main/java/org/boot/project/springboot/domain/posts/Posts.java
+++ src/main/java/org/boot/project/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package org.boot.project.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.boot.project.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
