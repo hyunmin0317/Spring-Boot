4f7895903afc6b41dccc40455a056885dbe9d7b9
hyunmin <choihm9903@naver.com>
2022-03-02 16:45:11 +0900

    feat: Google Login

--- src/main/java/org/boot/project/springboot/web/IndexController.java
+++ src/main/java/org/boot/project/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.boot.project.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.boot.project.springboot.config.auth.dto.SessionUser;
 import org.boot.project.springboot.service.posts.PostsService;
 import org.boot.project.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {  // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
