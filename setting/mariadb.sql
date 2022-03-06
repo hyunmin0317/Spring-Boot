50dd00c93435c56446a65b2295df918a8836d3b3
hyunmin <choihm9903@naver.com>
2022-03-06 11:02:54 +0900

    feat: EC2 RDS Connect (309p)

---
+++ setting/mariadb.sql
@@ -0,0 +1,43 @@
+CREATE TABLE posts (
+                       id BIGINT NOT NULL AUTO_INCREMENT,
+                       created_date DATETIME,
+                       modified_date DATETIME,
+                       author VARCHAR(255),
+                       context TEXT NOT NULL,
+                       title VARCHAR(500) NOT NULL,
+                       PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE user (
+                      id BIGINT NOT NULL AUTO_INCREMENT,
+                      created_date DATETIME,
+                      modified_date DATETIME,
+                      email VARCHAR(255) NOT NULL,
+                      name VARCHAR(255) NOT NULL,
+                      picture VARCHAR(255),
+                      role VARCHAR(255) NOT NULL,
+                      PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE SPRING_SESSION (
+                                PRIMARY_ID CHAR(36) NOT NULL,
+                                SESSION_ID CHAR(36) NOT NULL,
+                                CREATION_TIME BIGINT NOT NULL,
+                                LAST_ACCESS_TIME BIGINT NOT NULL,
+                                MAX_INACTIVE_INTERVAL INT NOT NULL,
+                                EXPIRY_TIME BIGINT NOT NULL,
+                                PRINCIPAL_NAME VARCHAR(100),
+                                CONSTRAINT SPRING_SESSION_PK PRIMARY KEY (PRIMARY_ID)
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
+
+CREATE UNIQUE INDEX SPRING_SESSION_IX1 ON SPRING_SESSION (SESSION_ID);
+CREATE INDEX SPRING_SESSION_IX2 ON SPRING_SESSION (EXPIRY_TIME);
+CREATE INDEX SPRING_SESSION_IX3 ON SPRING_SESSION (PRINCIPAL_NAME);
+
+CREATE TABLE SPRING_SESSION_ATTRIBUTES (
+                                           SESSION_PRIMARY_ID CHAR(36) NOT NULL,
+                                           ATTRIBUTE_NAME VARCHAR(200) NOT NULL,
+                                           ATTRIBUTE_BYTES BLOB NOT NULL,
+                                           CONSTRAINT SPRING_SESSION_ATTRIBUTES_PK PRIMARY KEY (SESSION_PRIMARY_ID, ATTRIBUTE_NAME),
+                                           CONSTRAINT SPRING_SESSION_ATTRIBUTES_FK FOREIGN KEY (SESSION_PRIMARY_ID) REFERENCES SPRING_SESSION(PRIMARY_ID) ON DELETE CASCADE
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
